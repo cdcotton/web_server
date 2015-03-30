43ff8a35648cabe2836d2001bb6ecdd89368f44a
Chris Cotton <chris.cottonn@gmail.com>
2015-03-29 21:22:36 -0700

    Added PUSH functionailty

--- browser.rb
+++ browser.rb
@@ -1,11 +1,34 @@
 require 'socket'
+require 'json'
 
 host = 'localhost'                  # The web server
 port = 2000                         # Default HTTP port
-path = "index.html"                 # The file we want
 
-# This is the HTTP request we send to fetch a file
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+def get_request
+  print 'What type of request would you like to send? ' \
+        "'GET' or 'PUSH?': "
+  method = gets.chomp.upcase
+  print 'Path: '
+  path = gets.chomp
+
+  case method
+  when 'GET'
+    return request = "GET #{path} HTTP/1.0\r\n\r\n"
+  when 'POST'
+    print 'Name: '
+    name = gets.chomp
+    print 'Email: '
+    email = gets.chomp
+
+    body = {:viking => {:name => name, :email => email}}.to_json
+
+    return request = "POST #{path} HTTP/1.0\r\n" \
+              "Content-Type: text/json\r\n" \
+              "Content-Length: #{body}.size\r\n\r\n#{body}"
+  end
+end
+
+request = get_request
 
 socket = TCPSocket.open(host, port) # Connect to server
 socket.print(request)               # Send request
--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 require 'socket'               # Get sockets from stdlib
+require 'json'
 
 server = TCPServer.open(2000)  # Socket to listen on port 2000
 
@@ -8,18 +9,29 @@ loop do                        # Servers run forever
 
   header, body = request.split("\r\n\r\n", 2)
   method = header.split[0]
-  fname = header.split[1][0..-1]
+  path = header.split[1][0..-1]
 
-  if File.exist?(fname)
-    body = File.read(fname)
+  if File.exist?(path)
+    body = File.read(path)
     client.puts("HTTP/1.0 200 OK\r\n\r\n")
+    response = ''
 
-    if method == 'GET'
-      client.puts(body)
+    case method
+    when 'GET'
+      response = body
+    when 'PUSH'
+      params = JSON.parse(body)
+      template = File.read('thanks.html')
+      content = "<li>Name: #{params['viking']['name']}</li>" \
+                "<li>Email: #{params['viking']['email']}</li>"
+      response = template.sub("<%= yield %>", content)
     end
   else
     client.puts("HTTP/1.0 404 Not Found\r\n\r\n")
     client.puts('You messed up.')
   end
+
+  client.puts(response)
+
   client.close                 # Disconnect from the client
 end
